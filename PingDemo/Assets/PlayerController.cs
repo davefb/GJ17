7bc290946ec5348ee5e7a7959c8bdb3d03635b5a
macfeilmeier <m.feilmeier8@gmail.com>
2017-01-21 13:10:46 -0800

    Mouse Input

---
+++ PingDemo/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+            if(hit.transform.gameObject.layer != 8)
+            {
+                hit.point = new Vector3(hit.point.x, 0, hit.point.z);
+                Debug.Log(hit.point);
+            }
+            else
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+                
+            }
+            transform.LookAt(hit.point);
+    }
+}
