1692050ee9f92518ccca5be7d2eac06bc8560ce1
davefb <dave.f.baldwin@gmail.com>
2017-01-22 12:46:31 -0800

    AudioManager and lighitng for keys

--- PingDemo/Assets/Scripts/AudioManager.cs
+++ PingDemo/Assets/Scripts/AudioManager.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour {
 	public Transform player;
 
-	AudioSource click;
+	AudioSource[] click;
 	public float bpm;
 	float delta;
+    float loopdelta;
 	float nextTime = 0;
+    float nextTimeLoop = 0;
+    float delay = 2f;
+    float cliptime = 8; int clipi = 0;
 	// Use this for initialization
 	void Start () {
-		click = GetComponent<AudioSource> ();
-		delta = bpm ;
-	}
+        click = GetComponentsInChildren<AudioSource>();
+		delta = 60.0f/bpm ;
+        loopdelta = 60.0f / (4*bpm);
+    }
 
 
-
+    bool first = true;
 	// Update is called once per frame
-	void Update () {
-		if (Time.timeSinceLevelLoad  > nextTime/60.0f) {
-			//click.PlayOneShot (click.clip);
+	void FixedUpdate () {
+
+        if (Time.timeSinceLevelLoad  > nextTime + delay) {
+   
+            if (Time.timeSinceLevelLoad > cliptime *clipi + delay )
+            {
+                Debug.Log("Playing....");
+                click[0].Stop();
+                click[0].Play();
+                clipi++;
+            }
+
+      //click[1].Stop();
+      //      click[1].Play();
 			nextTime += delta;
-			player.BroadcastMessage ("Tick");
+            //player.BroadcastMessage ("Tick");
+            GameObject.Find("Keys").BroadcastMessage("Tick");
+            player.root.BroadcastMessage("Tick");
             WallHitHandler.Tick();
+            
         }
 
-
+        //Debug.Log(Time.timeSinceLevelLoad + " " +  nextTime + " " + Time.timeSinceLevelLoad % cliptime);
 	}
 }
---
+++ PingDemo/Assets/Scripts/KeyHitHandler.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHitHandler : HitHandler {
+    enum State { INACTIVE, ACTIVE, HIT};
+    bool lightOn = false;
+    State currentState = State.INACTIVE;
+    public int[] activeBeats;
+    public Material hitColor;
+    public Material activeColor;
+    public Material inactiveColor;
+    int currentTick = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lightOn)
+        {
+            this.GetComponentInChildren<Light>().intensity = 1.5f;
+        }
+        else
+        {
+            this.GetComponentInChildren<Light>().intensity = 0f;
+        }
+    }
+    public void Tick()
+    {
+        bool found = false;
+        for(int i = 0; i < activeBeats.Length; i++)
+        {
+            if(currentTick%4 == activeBeats[i])
+            {
+                found = true;
+
+            
+            }
+
+            UpdateMaterial();
+        }
+
+        if (found)
+        {
+            currentState = State.ACTIVE;
+        }
+        else
+        {
+            currentState = State.INACTIVE;
+        }
+        currentTick++;
+    }
+    public override void HandleBeamHit(RaycastHit rch)
+    {
+        
+        base.HandleBeamHit(rch);
+        if (currentState == State.ACTIVE)
+        {
+            currentState = State.HIT;
+            Debug.Log("hit", this);
+            UpdateMaterial();
+        }
+
+
+    }
+   
+    public override void HandlePulseHit(RaycastHit rch)
+    {
+
+        base.HandleBeamHit(rch);
+        
+        StartCoroutine( offLight() );
+    }
+
+    IEnumerator offLight()
+    {
+        this.lightOn = true;
+        yield return new WaitForSeconds(2);
+        this.lightOn = false;
+    }
+    public void UpdateMaterial()
+    {
+        switch (currentState)
+        {
+            case State.HIT:
+                StartCoroutine(offLight());
+                GetComponent<Renderer>().material = hitColor;
+                break;
+            case State.ACTIVE:
+                // lightOn = false;
+                GetComponent<Renderer>().material = activeColor;
+                break;
+            case State.INACTIVE:
+                //lightOn = false;
+                GetComponent<Renderer>().material = inactiveColor;
+                break;
+
+        }
+
+    }
+}
