c45252fe2bfe1fc49054c222afb73b5599cf10e1
David Baldwin <dave.f.baldwin@gmail.com>
2017-01-21 16:30:33 -0800

    qf

---
+++ PingDemo/Assets/Scripts/WallHitHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitHandler : MonoBehaviour {
+	public GameObject glowParticles;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public override void HandlePulseHit(RaycastHit rch){
+		GameObject p = Instantiate (glowParticles);
+		p.transform.position = rch.point;
+		p.SetActive (true);
+	}
+
+	public override void HandleBeamHit(RaycastHit rch){
+
+	}
+}
